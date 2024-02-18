329664ab65edee8295e0fd5f11501517fcc23abc
Dunkelheit-afk <156872625+dunkelheit-afk@users.noreply.github.com>
2024-02-18 15:50:33 +0700

    homepage {fixed}

---
+++ HOMEPAGE/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage-Teman.AL</title>
+    <link rel="stylesheet" href="bagian/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css" rel="stylesheet"/>
+
+    <!-- buat bootstrap -->
+
+</head>
+<body>
+
+
+    <?php include 'bagian/header.html'?>
+    <?php include 'bagian/sidebar.html'?>
+    <?php include 'bagian/konten.html' ?>
+
+</body>
+</html>
